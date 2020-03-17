551d97c6fe8c293dc0364a0ad70d1077ff1c8111
Ali Ander <alifahriander@gmail.com>
2020-03-17 03:25:36 +0100

    optional modified

--- optionals/optional1/optional1.py
+++ optionals/optional1/optional1.py
@@ -15,18 +15,39 @@ from keras.optimizers import Adam, RMSprop
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
-lstm_base_model = load_model('best_models/best_model_lstm_mitbih_seeded_976.h5')
-lstm_base_model.summary()
+import os 
+import yaml 
+with open("paths.yaml",'r') as f :
+    paths = yaml.load(f, Loader=yaml.FullLoader)
+
+import argparse 
+parser = argparse.ArgumentParser()
+parser.add_argument('--model',type=str)
+args = parser.parse_args()
+
+
+model_name = paths['MITBIH']['Models'][args.model]
+print("Loading Model :",model_name)
+base_model = load_model(model_name)
+base_model.summary()
 
 # Now remove the last layers
 representation_model = Sequential()
-for layer in lstm_base_model.layers[:-3]: # go through until last lstm layer
+for layer in base_model.layers[:-3]: # go through until last lstm layer
     representation_model.add(layer)
 representation_model.summary()
 
 # feed the ptbdb data set and receive the representations
-df_1 = pd.read_csv("ptbdb_normal.csv", header=None)
-df_2 = pd.read_csv("ptbdb_abnormal.csv", header=None)
+
+
+
+
+path_normal = os.path.join(paths["PTDB"]["Data"], "ptbdb_normal.csv")
+path_abnormal = os.path.join(paths["PTDB"]["Data"], "ptbdb_abnormal.csv")
+
+
+df_1 = pd.read_csv(path_normal, header=None)
+df_2 = pd.read_csv(path_abnormal, header=None)
 df = pd.concat([df_1, df_2])
 
 df_train, df_test = train_test_split(df, test_size=0.2, random_state=1337, stratify=df[187])
@@ -58,15 +79,16 @@ model.add(BatchNormalization())
 model.add(Dense(64, activation='relu'))
 model.add(BatchNormalization())
 
-#model.add(Dense(64, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
 model.summary()
 opt = Adam(lr=0.001)
 model.compile(loss='binary_crossentropy', metrics=['acc'], optimizer=opt)
 
-checkpoint = ModelCheckpoint('best_model_optional1.h5', monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-early = EarlyStopping(monitor="val_acc", mode="max", patience=30, verbose=1)
+file_name = "optional1_"+args.model+".h5"
+print("Best model will be saved in ",file_name)
+checkpoint = ModelCheckpoint(file_name, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+early = EarlyStopping(monitor="val_acc", mode="max", patience=10, verbose=1)
 redonplat = ReduceLROnPlateau(monitor="val_acc", mode="max", patience=7, verbose=2, factor=0.5)
 
 model.fit(X_representations, Y, epochs=100, verbose=2, callbacks=[checkpoint, early, redonplat], validation_split=0.1,
--- optionals/optional2/optional2.py
+++ optionals/optional2/optional2.py
@@ -13,13 +13,31 @@ from keras.optimizers import Adam, RMSprop
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
-# load base rnn model
-lstm_base_model = load_model('best_models/best_model_lstm_mitbih_seeded_976.h5')
-lstm_base_model.summary()
+
+
+import os 
+import yaml 
+with open("paths.yaml",'r') as f :
+    paths = yaml.load(f, Loader=yaml.FullLoader)
+
+import argparse 
+parser = argparse.ArgumentParser()
+parser.add_argument('--model',type=str)
+args = parser.parse_args()
+
+
+model_name = paths['MITBIH']['Models'][args.model]
+print("Loading Model :",model_name)
+base_model = load_model(model_name)
+base_model.summary()
 
 # load the data
-df_1 = pd.read_csv("ptbdb_normal.csv", header=None)
-df_2 = pd.read_csv("ptbdb_abnormal.csv", header=None)
+path_normal = os.path.join(paths["PTDB"]["Data"], "ptbdb_normal.csv")
+path_abnormal = os.path.join(paths["PTDB"]["Data"], "ptbdb_abnormal.csv")
+
+
+df_1 = pd.read_csv(path_normal, header=None)
+df_2 = pd.read_csv(path_abnormal, header=None)
 df = pd.concat([df_1, df_2])
 
 df_train, df_test = train_test_split(df, test_size=0.2, random_state=1337, stratify=df[187])
@@ -34,7 +52,7 @@ X_test = np.array(df_test[list(range(187))].values)[..., np.newaxis]
 
 # Remove the last layers
 model = Sequential()
-for layer in lstm_base_model.layers[:-3]: # go through until last lstm layer
+for layer in base_model.layers[:-3]: # go through until last lstm layer
     model.add(layer)
 model.summary()
 
@@ -55,12 +73,13 @@ model.summary()
 opt = Adam(lr=0.001)
 model.compile(loss='binary_crossentropy', metrics=['acc'], optimizer=opt)
 
-
-checkpoint = ModelCheckpoint('best_model_optional2.h5', monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-early = EarlyStopping(monitor="val_acc", mode="max", patience=30, verbose=1)
+file_name = "optional2_"+args.model+".h5"
+print("Best model will be saved in ",file_name)
+checkpoint = ModelCheckpoint(file_name, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+early = EarlyStopping(monitor="val_acc", mode="max", patience=10, verbose=1)
 redonplat = ReduceLROnPlateau(monitor="val_acc", mode="max", patience=7, verbose=2, factor=0.5)
 
-model.fit(X, Y, epochs=100, verbose=2, callbacks=[checkpoint, early, redonplat], validation_split=0.1,
+model.fit(X, Y, epochs=1, verbose=2, callbacks=[checkpoint, early, redonplat], validation_split=0.1,
           batch_size=64)
 
 pred_test = model.predict(X_test)
